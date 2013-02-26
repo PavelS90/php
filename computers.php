63d43753af37086bfaea8ffc025ea1237dd1ea08
Sytnik Pavel <sytnik.p@gmail.com>
2013-02-26 12:30:58 +0200

    Update computers.php
    update

--- computers.php
+++ computers.php
@@ -10,7 +10,7 @@ $cut = "3";
 $var = (($inch / sqrt(pow(12,2)+pow(9,2)) * 12) * 2.54) / 10;
 $var = explode('.', $var);
 $solution .= $var[0].".";
-if(!empty($var[1])) { $solution .= substr($var[1], 0, 3); }
+if(!empty($var[1])) { $solution .= substr($var[1], 0, $cut); }
 $result = str_replace('.', ',', $solution);
 echo $result;
 ?>
