1b58573f43503717b89f6e36e3247636584e7469
PavelS90 <sytnik.p@gmail.com>
2013-02-26 10:20:57 +0200

    first commit

--- test.php
+++ test.php
@@ -8,6 +8,7 @@ $cut = "3";
 $var = substr(round((($inch / sqrt(pow(12,2)+pow(9,2)) * 12) * 2.54) / 10, 4), 0, -1);
 echo $var;
 
+#~
 #$var = (($inch / sqrt(pow(12,2)+pow(9,2)) * 12) * 2.54) / 10;
 #echo number_format($var, 3, '.', '');
 
