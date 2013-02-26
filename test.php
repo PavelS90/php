a1807c1ddec41b187c92f6197cffd5ea78cacd0a
PavelS90 <sytnik.p@gmail.com>
2013-02-26 12:02:48 +0200

    edit

--- test.php
+++ test.php
@@ -1,18 +1,17 @@
 <?php
 
+###variables
 $inch = "28";
 $one_side = "12";
 $two_side = "9";
 $cut = "3";
+###
 
-###result 1
-$var = substr(round((($inch / sqrt(pow(12,2)+pow(9,2)) * 12) * 2.54) / 10, 4), 0, -1);
-
-###result 2
-$vars = (($inch / sqrt(pow(12,2)+pow(9,2)) * 12) * 2.54) / 10;
-
-###print results
-echo $var . "<br>" . number_format($vars, 3, '.', '');
-
+$var = (($inch / sqrt(pow(12,2)+pow(9,2)) * 12) * 2.54) / 10;
+$var = explode('.', $var);
+$solution .= $var[0].".";
+if(!empty($var[1])) { $solution .= substr($var[1], 0, 3); }
+$result = str_replace('.', ',', $solution);
+echo $result;
 ?>
 
