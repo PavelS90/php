fc0c4e82ad2e32288113c8936e2d8d4d7740b6de
Sytnik Pavel <sytnik.p@gmail.com>
2013-02-26 18:37:08 +0200

    Update computers.php
    edit

--- computers.php
+++ computers.php
@@ -10,7 +10,7 @@ $cut = "3";
 $var = (($inch / sqrt(pow($one_side,2)+pow($sec_side,2)) * 12) * 2.54) / 10;
 $var = explode('.', $var);
 $solution .= $var[0].".";
-if(!empty($var[1])) { $solution .= substr($var[1], 0, 3); }
+if(!empty($var[1])) { $solution .= substr($var[1], 0, $cut); }
 $result = str_replace('.', ',', $solution);
 echo $result . " Дм.";
 
@@ -18,7 +18,7 @@ echo $result . " Дм.";
 
 
 ###tests
-#$one = substr(round((($inch / sqrt(pow(12,2)+pow(9,2)) * 12) * 2.54) / 10, 4), 0, -1);
+#$one = substr(round((($inch / sqrt(pow($one_side,2)+pow($sec_side,2)) * 12) * 2.54) / 10, 4), 0, -1);
 #$two = (($inch / sqrt(pow(12,2)+pow(9,2)) * 12) * 2.54) / 10;
 ###print tests
 #echo $one . " Дм. <br>" . number_format($two, $cut, '.', '') . " Дм.";
