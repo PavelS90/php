362208ed581a0b38fbf762be3214ba75a8d57545
PavelS90 <sytnik.p@gmail.com>
2013-02-26 11:25:14 +0200

    edit

--- test.php
+++ test.php
@@ -5,12 +5,14 @@ $one_side = "12";
 $two_side = "9";
 $cut = "3";
 
+###result 1
 $var = substr(round((($inch / sqrt(pow(12,2)+pow(9,2)) * 12) * 2.54) / 10, 4), 0, -1);
-echo $var;
 
-#~
-#$var = (($inch / sqrt(pow(12,2)+pow(9,2)) * 12) * 2.54) / 10;
-#echo number_format($var, 3, '.', '');
+###result 2
+$vars = (($inch / sqrt(pow(12,2)+pow(9,2)) * 12) * 2.54) / 10;
+
+###print results
+echo $var . "<br>" . number_format($vars, 3, '.', '');
 
 ?>
 
