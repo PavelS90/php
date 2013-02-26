2a77e49929199c9e90e1f16c62e21bc2a51dcd65
Sytnik Pavel <sytnik.p@gmail.com>
2013-02-26 15:12:57 +0200

    Update computers.php
    edit

--- computers.php
+++ computers.php
@@ -3,11 +3,11 @@
 ###variables
 $inch = "28";
 $one_side = "12";
-$two_side = "9";
+$sec_side = "9";
 $cut = "3";
 ###
 
-$var = (($inch / sqrt(pow(12,2)+pow(9,2)) * 12) * 2.54) / 10;
+$var = (($inch / sqrt(pow($one_side,2)+pow($sec_side,2)) * 12) * 2.54) / 10;
 $var = explode('.', $var);
 $solution .= $var[0].".";
 if(!empty($var[1])) { $solution .= substr($var[1], 0, 3); }
